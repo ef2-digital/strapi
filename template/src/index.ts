e613b35ff6f5ff897bb38aee10881e44d35ae1fb
bitbucket-bartkoelewijnef2 <bartkoelewijn@ef2.nl>
2022-10-07 11:06:02 +0200

    homepage subcomponent attempt

--- template/src/index.ts
+++ template/src/index.ts
@@ -24,6 +24,21 @@ export default {
       strapi.entityService.create('api::homepage.homepage', {
         data: {
           title: 'Rediscover to renew',
+          content: [
+            {
+              __component: 'content.image-texts',
+              direction: 'odd',
+              images: [
+                {
+                  __component: 'content.image-text',
+                  title: 'Waar ga jij beginnen?',
+                  subtitle: 'Creative Digital Agency',
+                  description:
+                    'EF2 maakt jouw verbeterslag realiteit. Wij helpen je op een pragmatische manier, met een compleet plan. Gebaseerd op eenvoud. Eerst kijken wat er goed gaat. Kansen inventariseren. Dan maken we een sprong vooruit. Zo kun jij groeien vanuit je bestaande, solide basis. Dat is Rediscover to Renew!',
+                },
+              ],
+            },
+          ],
         },
       });
     }
