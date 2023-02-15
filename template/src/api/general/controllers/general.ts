e6429011bffb6e5fbaf22d3d87c0fab255eab9fd
github-bartkoelewijnnl <bartkoelewijnnl@gmail.com>
2023-02-15 14:11:20 +0100

    Template update

--- template/src/api/general/controllers/general.ts
+++
@@ -1,7 +0,0 @@
-/**
- * general controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::general.general');
--- template/src/api/general/routes/general.ts
+++
@@ -1,7 +0,0 @@
-/**
- * general router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::general.general');
