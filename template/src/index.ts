7ab473942a418f761c20c938922bfdc46cd04c48
Daan de Gooijer <daan@ef2.nl>
2023-04-19 11:33:07 +0200

    Added bootstrap permissions

---
+++ template/src/index.ts
@@ -0,0 +1,40 @@
+export default {
+    /**
+     * An asynchronous register function that runs before
+     * your application is initialized.
+     *
+     * This gives you an opportunity to extend code.
+     */
+    register(/*{ strapi }*/) {},
+
+    /**
+     * An asynchronous bootstrap function that runs before
+     * your application gets started.
+     *
+     * This gives you an opportunity to set up your data model,
+     * run jobs, or perform some special logic.
+     */
+    async bootstrap({ strapi }) {
+        await strapi.admin.services.role.createRolesIfNoneExist();
+
+        const findPublicRole = async () => {
+            const result = await strapi.service('plugin::users-permissions.role').find({});
+
+            return Object.values(result)
+                .filter((role) => role.type === 'public')
+                .pop().id;
+        };
+
+        const publicRole = await strapi.service('plugin::users-permissions.role').findOne(await findPublicRole(), {});
+
+        for (const permission of Object.keys(publicRole.permissions)) {
+            if (permission.startsWith('api')) {
+                for (const controller of Object.keys(publicRole.permissions[permission].controllers)) {
+                    publicRole.permissions[permission].controllers[controller].find.enabled = true;
+                }
+            }
+        }
+
+        await strapi.service('plugin::users-permissions.role').updateRole(publicRole.id, publicRole);
+    }
+};
